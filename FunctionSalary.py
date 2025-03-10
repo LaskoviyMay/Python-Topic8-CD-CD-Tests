0b7a52f18acc45f933a33a9ff96bc86ca9a7d4c7
LaskoviyMay <46874404+LaskoviyMay@users.noreply.github.com>
2025-03-10 19:47:25 +0300

    Update FunctionSalary.py

--- FunctionSalary.py
+++ FunctionSalary.py
@@ -1,18 +1,28 @@
 def calculate_salary(base_salary, cargo_coefficient, cargo_volume, bonuses, deductions):
+    """
+    Function to calculate month salary 
 
+    """
+    # Calculate salary
     salary = base_salary + (cargo_coefficient * cargo_volume) + bonuses - deductions
 
+    # Check salary
     if salary < base_salary:
         salary = base_salary
 
+    # Round salary
     return round(salary, 2)
 
+
+# Parameters
 base_salary = 10000
 cargo_coefficient = 0.01
 cargo_volume = 700000
 bonuses = 500
 deductions = 200
 
+# Calculate month salary
 month_salary = calculate_salary(base_salary, cargo_coefficient, cargo_volume, bonuses, deductions)
 
+# Print result
 print(f"Salary is: {month_salary} $")
